c184dd31fb3b22bab3f34d4dee895056534c5b8e
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2019-07-08 22:51:24 -0500

    actually accomplish goal

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,18 +1,40 @@
 port module Main exposing (main)
 
 import Browser
-import Html
+import Html exposing (button, text)
+import Html.Events exposing (onClick)
+
 
 port outgoing : String -> Cmd msg
 
-main : Program () () ()
+
+type alias Model =
+    Bool
+
+
+type Msg
+    = Clicky
+
+
+main : Program () Model Msg
 main =
     Browser.element
-        { init = init
-        , update = \msg model -> (model, Cmd.none)
-        , view = always (Html.text "Hello world!")
+        { init = always ( False, Cmd.none )
+        , update = update
+        , view = view
         , subscriptions = always Sub.none
         }
-    
-init _ =
-    ((), outgoing "")
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg _ =
+    ( True, outgoing "clicky" )
+
+
+view model =
+    if model then
+        text "connected!"
+
+    else
+        button [ onClick Clicky ]
+            [ Html.text "Hello world!" ]
